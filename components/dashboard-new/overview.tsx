087cbd1c1f6655873e92565b010200a4fcc43320
Fawad Shah <fawadshah.thall@gmail.com>
2024-10-31 01:59:14 +0300

    adding test commit

--- components/dashboard-new/overview.tsx
+++ components/dashboard-new/overview.tsx
@@ -71,11 +71,7 @@ export default function DashboardOverview() {
   return (
     <div className="flex-col md:flex mb-5">
       <div className="">
-        <div className="flex items-center justify-between space-y-2">
-          {/*<h2 className="text-3xl font-bold tracking-tight">*/}
-          {/*  UtillTise Dashboard*/}
-          {/*</h2>*/}
-        </div>
+        <div className="flex items-center justify-between space-y-2"></div>
         <div className="grid gap-4 md:grid-cols-4 lg:grid-cols-8">
           <Card className="col-span-2 row-span-2">
             <CardHeader>
