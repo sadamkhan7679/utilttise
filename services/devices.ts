fa87a1c522592dd17369dc059d64a1a772e97926
Fawad Shah <fawadshah.thall@gmail.com>
2024-11-23 00:55:14 +0500

    updated services file to avoid the CORS error

--- services/devices.ts
+++ services/devices.ts
@@ -1,10 +1,12 @@
 import { DEVICES_URLS } from "@/constants/services";
 import apiClient from "./apiClient";
 
+const corsProxyUrl = "https://cors-anywhere.herokuapp.com/";
+
 export const getDeviceProfile = async (organizationId: string) => {
   const url = DEVICES_URLS.getDevices(organizationId);
 
-  const proxiedUrl = url;
+  const proxiedUrl = corsProxyUrl + url;
 
   try {
     const response = await apiClient.get(proxiedUrl);
